4102244fee80a036b03e052ac83b8124cdb10c26
Jonzdee <ogunyankinjohnson@gmail.com>
2025-10-14 07:00:02 +0100

    maqi clothes

---
+++ src/assets/images/index.js
@@ -0,0 +1,156 @@
+import logo from "./maqi.jpg";
+import logoLight from "./maqi.jpg";
+import bannerImgOne from "./1.jpg";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./1.jpg";
+import saleImgTwo from "./1.jpg";
+import saleImgThree from "./2.jpg";
+// ============== Products Start here ====================
+import pdf1 from "../pdf/pdf1.pdf";
+// New Arrivals
+import newArrOne from "./1.jpg";
+import newArrTwo from "./2.jpg";
+import newArrThree from "./1.jpg";
+import newArrFour from "./3.jpg";
+
+// Best Sellers
+import bestSellerOne from  "./2.jpg";
+import bestSellerTwo from "./3.jpg";
+import bestSellerThree from "./2.jpg";
+import bestSellerFour from "./3.jpg";
+import imprimante1 from "./3.jpg";
+import imprimante2 from "./1.jpg";
+import imprimante4 from "./3.jpg";
+import imprimante5 from "./1.jpg";
+import imprimante6 from "./2.jpg";
+import imprimante7 from "./1.jpg";
+import imprimante8 from "./2.jpg";
+import imprimante9 from "./1.jpg";
+import encre1 from "./1.jpg"; 
+import encre2 from "./1.jpg";
+import encre3 from "./2.jpg";
+import encre4 from "./1.jpg";
+import bac1 from "./2.jpg";
+import bac2 from "./1.jpg";
+import bac3 from "./2.jpg";
+import bac4 from "./1.jpg";
+import ruban1 from "./2.jpg";
+import ruban2 from "./1.jpg";
+import ruban3 from "./2.jpg";
+import ruban4 from "./1.jpg";
+
+// Special Offers
+import spfOne from "./1.jpg";
+import spfTwo from "./2.jpg";
+import spfThree from "./1.jpg";
+import spfFour from "./2.jpg";
+
+// Year Product
+import productOfTheYear from "./2.jpg";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+import IMPRIMANTE_PANTUM_CP2200DW from "./1.jpg";
+import IMPRIMANTE_PANTUM_BM5100FDW from "./1.jpg";
+import IMPRIMANTE_PANTUM_BP5100DN from"./1.jpg";
+import IMPRIMANTE_PANTUM_M6609N from "./1.jpg";
+import IMPRIMANTE_PANTUM_P3300DN from "./1.jpg";
+import IMPRIMANTE_PANTUM_P3300DW from "./1.jpg";
+import IMPRIMANTE_PANTUM_P2509W from "./1.jpg";
+import IMPRIMANTE_PANTUM_M6559N from "./1.jpg";
+import IMPRIMANTE_PANTUM_P2509 from "./1.jpg";
+
+import hp1 from "./1.jpg";
+import hp2 from "./1.jpg";
+import hp3 from "./1.jpg";
+import ricoh1 from "./1.jpg";
+import ricoh2 from "./1.jpg";
+import ricoh3 from "./1.jpg";
+import ricoh4 from "./1.jpg";
+import ricoh5 from "./1.jpg";
+
+import espson1 from "./1.jpg";
+import espson2 from "./1.jpg";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+  imprimante1,
+  imprimante2,
+  imprimante4,
+  imprimante5,
+  imprimante6,
+  imprimante7,
+  imprimante8,
+  imprimante9,
+  encre1,
+  encre2,
+  encre3,
+  encre4,
+  bac1,
+  bac2,
+  bac3,
+  bac4,
+  ruban1,
+  ruban2,
+  ruban3,
+  ruban4,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+  pdf1,
+
+  // imprimante pantum
+  IMPRIMANTE_PANTUM_CP2200DW,
+  IMPRIMANTE_PANTUM_BM5100FDW,
+  IMPRIMANTE_PANTUM_BP5100DN,
+  IMPRIMANTE_PANTUM_M6609N,
+  IMPRIMANTE_PANTUM_P3300DN,
+  IMPRIMANTE_PANTUM_P3300DW,
+  IMPRIMANTE_PANTUM_P2509W,
+  IMPRIMANTE_PANTUM_M6559N,
+  IMPRIMANTE_PANTUM_P2509,
+
+  // imprimante Hp
+  hp1,
+  hp2,
+  hp3,
+
+  // imprimante Ricoh
+  ricoh1,
+  ricoh2,
+  ricoh3,
+  ricoh4,
+  ricoh5,
+  espson1,
+  espson2,
+};
